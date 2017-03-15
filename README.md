c476684448f2570dabe1547c81cca334c267f5cd
andynet <andrejbalaz001@gmail.com>
2017-03-15 23:44:40 +0100

    changed README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # algotrading
+
+purpose of this project is to learn some basics of machine learning in python with scikit
--- machine_learning.py
+++ machine_learning.py
@@ -1,8 +1,11 @@
 import functions
 import sklearn
+import v20
+import const
 
 buying_prices, selling_prices = functions.read_input("EUR_USD.data")
 orders = functions.find_best_points(buying_prices, selling_prices)
 
-for i in range(50, len(buying_prices))
+for i in range(50, len(buying_prices)):
     features = buying_prices
+    # TODO
