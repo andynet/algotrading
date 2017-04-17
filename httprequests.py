6bb9480c76701c6ffbecb490373e228dcee16728
andynet <andrejbalaz001@gmail.com>
2017-04-17 14:39:44 +0200

    created first functional orders via requests

--- httprequests.py
+++ httprequests.py
@@ -1,9 +1,22 @@
-import httplib
-import httplib2
-import urllib
-import urllib3
+import requests
 import json
+import constants
+import sys
 
-https: // github.com / oanda / py - position - aggregation / blob / master / aggregate_positions.py
-http: // developer.oanda.com / rest - live - v20 / order - ep /
-http: // stackoverflow.com / questions / 3305250 / python - urllib - vs - httplib
+# documentation at http://developer.oanda.com/rest-live-v20/introduction/
+
+url = 'https://{}/v3/accounts/{}/orders'.format(constants.HOSTNAME, constants.ACCOUNT_ID)
+
+header = {'Content-Type': 'application/json',
+          'Authorization': 'Bearer {}'.format(constants.TOKEN)}
+
+body = {'order': ({'instrument': 'EUR_USD', 'units': 100, 'type': 'MARKET'})}
+
+json1 = json.JSONEncoder().encode(body)
+# json.dump(json1, sys.stdout, indent=2)
+
+r = requests.post(url, data=json1, headers=header)
+json.dump(r.json(), sys.stdout, indent=2)
+
+r = requests.get(url, headers=header)
+# json.dump(r.json(), sys.stdout, sort_keys=True, indent=2)
