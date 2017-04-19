8a57d82b3770f8994c41435e38f287073fc8dc99
andynet <andrejbalaz001@gmail.com>
2017-04-19 20:10:09 +0200

    added timing to collector output

--- collector.py
+++ collector.py
@@ -1,12 +1,11 @@
 import time
 import api_functions
 
-# TODO add time to records?
 # TODO write output file format specification
 
 while True:
     buy_price, sell_price = api_functions.get_price()
-    line = buy_price + ' ' + sell_price + '\n'
+    line = buy_price + ' ' + sell_price + ' ' + time.ctime() + '\n'
 
     with open('EUR_USD.collected', 'a') as f:
         f.write(line)
